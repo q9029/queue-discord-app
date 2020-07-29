3bfb4420bd6d2f71ec8bd3b4ab416fee5a61cbd0
Queue <key.ken0215@outlook.jp>
2020-07-30 05:12:03 +0900

    first

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ psql.connect();
     psql.query("select * from guardians where name = 'アルテミス' order by name;", (err, res) => {
       if (err) throw err;
       for (let row of res.rows) {
-        console.log("[" + row.rarity + "] " + row.name + "\r\n\r\n○通常スキル" + row.damege + " CT：" + row.recast + "\r\n" + row.skill + "\r\n\r\n○編成スキル" + row.support);
+        console.log("[" + row.rarity + "] " + row.name + "\r\n\r\n○通常スキル\r\n" + row.damege + " CT：" + row.recast + "\r\n" + row.skill + "\r\n\r\n○編成スキル\r\n" + row.support);
       }
     });
 
