2ea563473291969ced335e24368ea2f4ea929766
Queue <key.ken0215@outlook.jp>
2020-07-30 04:25:52 +0900

    first

--- index.js
+++ index.js
@@ -1,32 +1,42 @@
-const express = require('express')
-const port = process.env.PORT || 5000
+// herokuはポートリッスンしないと60sで落とされる
+const express = require('express');
+const port = process.env.PORT || 5000;
 express().listen(port, function(){
     console.log("Listen!");
 });
 
-const Discord = require("discord.js");
+// 非公開情報は環境変数から取得する
 const token = process.env.TOKEN;
+
+const pg = require('pg');
+pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  client.query('select * from guardians order by name', function(err, result) {
+    done();
+    if(err) return console.error(err);
+    console.log(result.rows);
+  });
+});
+
+const Discord = require("discord.js");
 const ytdl = require("ytdl-core");
 
-const queue = new Map();
-
 const client = new Discord.Client();
-
 client.once("ready", () => {
   console.log("Ready!");
 });
-
 client.once("reconnecting", () => {
   console.log("Reconnecting!");
 });
-
 client.once("disconnect", () => {
   console.log("Disconnect!");
 });
 
+// メッセージ受信時
 client.on("message", async message => {
+
+  // 投稿者がbotは無視する
   if (message.author.bot) return;
-  const serverQueue = queue.get(message.guild.id);
+
   if (message.content.indexOf("にんじん") != -1) {
     message.channel.send("たべたい");
     return;
@@ -34,3 +44,4 @@ client.on("message", async message => {
 });
 
 client.login(token);
+	
