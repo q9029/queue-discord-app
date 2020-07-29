0b2ce33660d06506d04e2e8fe6296892528afc7d
Queue <key.ken0215@outlook.jp>
2020-07-30 05:35:38 +0900

    first

--- index.js
+++ index.js
@@ -55,16 +55,14 @@ client.on("message", async message => {
     return;
   }
 
-  if (message.isMemberMentioned(client.user)) {
+  if (message.content.startsWith("おしえて ")) {
     const args = message.content.split(" ");
-    if (args.length > 1) {
-      psql.query("select * from guardians where name = '" + args[1] + "' order by name;", (err, res) => {
-        if (err) throw err;
-        for (let row of res.rows) {
-          message.channel.send("[" + row.rarity + "] " + row.name + "\r\n\r\n○通常スキル\r\n" + row.damege + " CT：" + row.recast + "\r\n" + row.skill + "\r\n\r\n○編成スキル\r\n" + row.support);
-        }
-      });
-    }
+    psql.query("select * from guardians where name = '" + args[1] + "' order by name;", (err, res) => {
+      if (err) throw err;
+      for (let row of res.rows) {
+        message.channel.send("[" + row.rarity + "] " + row.name + "\r\n\r\n○通常スキル\r\n" + row.damege + " CT：" + row.recast + "\r\n" + row.skill + "\r\n\r\n○編成スキル\r\n" + row.support);
+      }
+    });
     return;
   }
 });
