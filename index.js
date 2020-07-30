7c0fa6d52a6ec1b04ad74f3af930150788d8531e
Queue <key.ken0215@outlook.jp>
2020-07-30 23:47:28 +0900

    first

--- index.js
+++ index.js
@@ -73,14 +73,14 @@ client.login(process.env.TOKEN);
 // herokuは1時間アクセスがないと落とされる
 const https = require('https');
 while (true) {
-  const d1 = new Date();
-  while (true) {
-    const d2 = new Date();
-    if (d2 - d1 > 60000) {
-      return;
-    }
-  }
   https.get('https://queue-discord-app.herokuapp.com/', function(res) {
     console.log("Get!");
   });
+  const d1 = new Date();
+  while (true) {
+    const d2 = new Date();
+    if (d2 - d1 > 60) {
+      return;
+    }
+  }
 }
