4e98622170957d8a13fc7c5a9e6b681798f2d1c9
Queue <key.ken0215@outlook.jp>
2020-08-03 04:11:09 +0900

    thread test

---
+++ _index.js
@@ -0,0 +1,89 @@
+// herokuはポートリッスンしないと60sで落とされる
+const express = require("express");
+const port = process.env.PORT || 5000;
+express().listen(port, function(){
+    console.log("Listen!");
+});
+
+// postgresqlに接続する
+const { Client } = require("pg");
+const psql = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+psql.connect();
+
+/*
+psql.query("select * from guardians where name = 'アルテミス' order by name;", (err, res) => {
+if (err) throw err;
+  for (let row of res.rows) {
+    console.log("[" + row.rarity + "] " + row.name + "\r\n\r\n○通常スキル\r\n" + row.damege + " CT：" + row.recast + "\r\n" + row.skill + "\r\n\r\n○編成スキル\r\n" + row.support);
+  }
+});
+*/
+
+// discordに接続する
+const Discord = require("discord.js");
+const ytdl = require("ytdl-core");
+
+const client = new Discord.Client();
+client.once("ready", () => {
+  console.log("Ready!");
+});
+client.once("reconnecting", () => {
+  console.log("Reconnecting!");
+});
+client.once("disconnect", () => {
+  console.log("Disconnect!");
+});
+
+// リスナー定義
+client.on("message", async message => {
+
+  // 投稿者がbotは無視する
+  if (message.author.bot) return;
+
+  if (message.content.indexOf("にんじん") != -1) {
+    message.channel.send("たべたい");
+    return;
+  }
+  if (message.content.indexOf("敗北者") != -1) {
+    message.channel.send("取り消せよ……!! ハァ… 今の言葉……!!!");
+    return;
+  }
+
+  if (message.content.startsWith("おしえて ")) {
+    const args = message.content.split(" ");
+    if (args.length > 1) {
+      psql.query("select * from guardians where name = '" + args[1] + "' order by name;", (err, res) => {
+        if (err) throw err;
+        for (let row of res.rows) {
+          message.channel.send("[" + row.rarity + "] " + row.name + "\r\n\r\n○通常スキル\r\n" + row.damege + " CT：" + row.recast + "\r\n" + row.skill + "\r\n\r\n○編成スキル\r\n" + row.support);
+        }
+      });
+    }
+    return;
+  }
+});
+
+client.login(process.env.TOKEN);
+
+/*
+TODO 別スレッド化
+// herokuは1時間アクセスがないと落とされる
+const https = require('https');
+while (true) {
+  https.get('https://queue-discord-app.herokuapp.com/', function(res) {
+    console.log("Get!");
+  });
+  const d1 = new Date();
+  while (true) {
+    const d2 = new Date();
+    if (d2 - d1 > 60) {
+      break;
+    }
+  }
+}
+*/
--- index.js
+++ index.js
@@ -1,78 +1,15 @@
-// herokuはポートリッスンしないと60sで落とされる
-const express = require("express");
-const port = process.env.PORT || 5000;
-express().listen(port, function(){
-    console.log("Listen!");
-});
+const { exec } = require('child_process');
 
-// postgresqlに接続する
-const { Client } = require("pg");
-const psql = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-psql.connect();
-
-/*
-psql.query("select * from guardians where name = 'アルテミス' order by name;", (err, res) => {
-if (err) throw err;
-  for (let row of res.rows) {
-    console.log("[" + row.rarity + "] " + row.name + "\r\n\r\n○通常スキル\r\n" + row.damege + " CT：" + row.recast + "\r\n" + row.skill + "\r\n\r\n○編成スキル\r\n" + row.support);
-  }
-});
-*/
-
-// discordに接続する
-const Discord = require("discord.js");
-const ytdl = require("ytdl-core");
-
-const client = new Discord.Client();
-client.once("ready", () => {
-  console.log("Ready!");
-});
-client.once("reconnecting", () => {
-  console.log("Reconnecting!");
-});
-client.once("disconnect", () => {
-  console.log("Disconnect!");
-});
-
-// リスナー定義
-client.on("message", async message => {
-
-  // 投稿者がbotは無視する
-  if (message.author.bot) return;
-
-  if (message.content.indexOf("にんじん") != -1) {
-    message.channel.send("たべたい");
-    return;
-  }
-  if (message.content.indexOf("敗北者") != -1) {
-    message.channel.send("取り消せよ……!! ハァ… 今の言葉……!!!");
-    return;
-  }
-
-  if (message.content.startsWith("おしえて ")) {
-    const args = message.content.split(" ");
-    if (args.length > 1) {
-      psql.query("select * from guardians where name = '" + args[1] + "' order by name;", (err, res) => {
-        if (err) throw err;
-        for (let row of res.rows) {
-          message.channel.send("[" + row.rarity + "] " + row.name + "\r\n\r\n○通常スキル\r\n" + row.damege + " CT：" + row.recast + "\r\n" + row.skill + "\r\n\r\n○編成スキル\r\n" + row.support);
-        }
-      });
+exec('ls -l', (err, stdout, stderr) => {
+    if (err) {
+      console.log(`stderr: ${stderr}`);
+      return
     }
-    return;
+    console.log(`stdout: ${stdout}`);
   }
-});
+)
+console.log('わくわくBank');
 
-client.login(process.env.TOKEN);
-
-/*
-TODO 別スレッド化
-// herokuは1時間アクセスがないと落とされる
 const https = require('https');
 while (true) {
   https.get('https://queue-discord-app.herokuapp.com/', function(res) {
@@ -86,4 +23,3 @@ while (true) {
     }
   }
 }
-*/
